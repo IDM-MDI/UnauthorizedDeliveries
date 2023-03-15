ee2ae853cd3c6b66974db0ec71e1aa243f71a867
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-15 14:32:38 +0300

    Initial commit

---
+++ src/main/java/by/a1/unauthorizeddeliveries/UnauthorizedDeliveriesApplication.java
@@ -0,0 +1,13 @@
+package by.a1.unauthorizeddeliveries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnauthorizedDeliveriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnauthorizedDeliveriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/a1/unauthorizeddeliveries/UnauthorizedDeliveriesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.a1.unauthorizeddeliveries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnauthorizedDeliveriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
