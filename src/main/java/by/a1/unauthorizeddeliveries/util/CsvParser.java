40b0a4085c080ce4d9afbc135efa656f7f3e5dc8
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-15 15:41:45 +0300

    edit dependency

--- src/main/java/by/a1/unauthorizeddeliveries/util/CsvParser.java
+++ src/main/java/by/a1/unauthorizeddeliveries/util/CsvParser.java
@@ -1,5 +1,22 @@
 package by.a1.unauthorizeddeliveries.util;
 
-public interface CsvParser {
+import by.a1.unauthorizeddeliveries.model.CsvLoginModel;
+import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public interface CsvParser<T> {
+    List<T> parse(String path) throws IOException;
+    default List<T> parse(String path, Class<T> clazz) throws IOException {
+        try (Reader reader = Files.newBufferedReader(Path.of(path))) {
+            return new CsvToBeanBuilder<T>(reader)
+                    .withType(clazz)
+                    .build()
+                    .parse();
+        }
+    }
 }
--- src/main/java/by/a1/unauthorizeddeliveries/util/impl/CsvLoginParser.java
+++ src/main/java/by/a1/unauthorizeddeliveries/util/impl/CsvLoginParser.java
@@ -1,6 +1,16 @@
 package by.a1.unauthorizeddeliveries.util.impl;
 
+import by.a1.unauthorizeddeliveries.model.CsvLoginModel;
 import by.a1.unauthorizeddeliveries.util.CsvParser;
+import org.springframework.stereotype.Component;
 
-public class CsvLoginParser implements CsvParser {
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class CsvLoginParser implements CsvParser<CsvLoginModel> {
+    @Override
+    public List<CsvLoginModel> parse(String path) throws IOException {
+        return parse(path,CsvLoginModel.class);
+    }
 }
