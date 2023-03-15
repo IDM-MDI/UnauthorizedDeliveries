7ef1af65b35473c0e8fc04fa78e6a49d741b040d
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-15 15:11:02 +0300

    add posting model

---
+++ src/main/java/by/a1/unauthorizeddeliveries/util/model/CsvPostingModel.java
@@ -0,0 +1,34 @@
+package by.a1.unauthorizeddeliveries.util.model;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CsvPostingModel {
+    @CsvBindByName(column = "Mat. Doc.")
+    private long id;
+    @CsvBindByName(column = "Item")
+    private long item;
+    @CsvBindByName(column = "Doc. Date")
+    private LocalDate contractDate;
+    @CsvBindByName(column = "Pstng Date")
+    private LocalDate postingDate;
+    @CsvBindByName(column = "Material Description")
+    private String materialDescription;
+    @CsvBindByName(column = "Quantity")
+    private int quantity;
+    @CsvBindByName(column = "BUn")
+    private String measurementUnit;
+    @CsvBindByName(column = "Amount LC")
+    private long amount;
+    @CsvBindByName(column = "Crcy")
+    private String currency;
+    @CsvBindByName(column = "User Name")
+    private String username;
+}
