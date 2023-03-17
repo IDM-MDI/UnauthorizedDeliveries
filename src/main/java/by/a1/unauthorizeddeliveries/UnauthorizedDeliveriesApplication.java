37de52d81cac537dba52556d7c7339f841417064
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-17 03:30:32 +0300

    add initial parse from csv

--- src/main/java/by/a1/unauthorizeddeliveries/UnauthorizedDeliveriesApplication.java
+++ src/main/java/by/a1/unauthorizeddeliveries/UnauthorizedDeliveriesApplication.java
@@ -1,17 +1,40 @@
 package by.a1.unauthorizeddeliveries;
 
+import by.a1.unauthorizeddeliveries.service.ParseService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class UnauthorizedDeliveriesApplication {
+    private final Environment env;
+    private final List<ParseService> parseServices;
     public static void main(String[] args) {
         SpringApplication.run(UnauthorizedDeliveriesApplication.class, args);
     }
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialParse() throws IOException {
+        if(Arrays.asList(env.getActiveProfiles())
+                .contains("test")) {
+            return;
+        }
+        for (ParseService parseService : parseServices) {
+            parseService.parseToDB();
+        }
     }
 }
